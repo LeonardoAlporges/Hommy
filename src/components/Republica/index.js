dfd7f4964922e0d19baf46ca61aff84047f69cdd
Robinson <robinsonlc201@gmail.com>
2020-05-14 12:13:20 -0300

    filtro republica

--- src/components/Republica/index.js
+++ src/components/Republica/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { ScrollView, FlatList, View } from 'react-native';
+import { ScrollView, FlatList, View, Modal, Text, TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
+import { CheckBox, ListItem, Fab, Icon, Input, Item } from 'native-base';
 
 import estilosRepublica from './style';
 
 import Cartao from '../Cartao/index';
 import api from '../../service/api';
+import _ from "lodash";
+var aluguelMin = null;
+var aluguelMax = null;
+
 
 class Republica extends Component {
   static navigationOptions = { header: null };
@@ -13,6 +18,25 @@ class Republica extends Component {
     super(props);
     this.state = {
       listaRepublicas: [],
+      fullData: [],
+      modalVisible: false,
+      filtroAnimalSim: false,
+      filtroAnimalNao: false,
+      filtroMQuarto: false,
+      filtroMasc: false,
+      filtroFem: false,
+      filtroMista: false,
+      filtroMoradores2: false,
+      filtroMoradores3: false,
+      filtroMoradores4: false,
+      filtroMoradores5: false,
+      filtroMoradores6: false,
+      filtroVagas1: false,
+      filtroVagas2: false,
+      filtroVagas3: false,
+      loading: false,
+      filtroValorMenor: false,
+      filtroValorMaior: false,
     };
   }
 
@@ -20,13 +44,226 @@ class Republica extends Component {
     return api
       .get('/main')
       .then(responseJson => {
-        this.setState({ listaRepublicas: responseJson.data });
+        console.log(responseJson);
+        this.setState({
+          listaRepublicas: responseJson.data,
+          fullData: responseJson.data,
+          loading: false
+        });
+
       })
       .catch(error => {
         console.error('SERVIDOR ESTA DESLIGADO');
       });
   }
 
+  fAnimalSim = async (checked) => {
+    if (this.state.filtroAnimalSim)
+      await this.setState({ filtroAnimalSim: false });
+    else
+      await this.setState({ filtroAnimalSim: true, filtroAnimalNao: false });
+  }
+  fAnimalNao = async (checked) => {
+    if (this.state.filtroAnimalNao)
+      await this.setState({ filtroAnimalNao: false });
+    else
+      await this.setState({ filtroAnimalNao: true, filtroAnimalSim: false });
+  }
+
+  fMasc = async (checked) => {
+    if (this.state.filtroMasc)
+      await this.setState({ filtroMasc: false });
+    else
+      await this.setState({ filtroMasc: true, filtroFem: false, filtroMista: false });
+  }
+
+  fFem = async (checked) => {
+    if (this.state.filtroFem)
+      await this.setState({ filtroFem: false });
+    else
+      await this.setState({ filtroFem: true, filtroMasc: false, filtroMista: false });
+  }
+
+  fMista = async (checked) => {
+    if (this.state.filtroMista)
+      await this.setState({ filtroMista: false });
+    else
+      await this.setState({ filtroMista: true, filtroMasc: false, filtroFem: false });
+  }
+
+  fMoradores2 = async (checked) => {
+    if (this.state.filtroMoradores2)
+      await this.setState({ filtroMoradores2: false });
+    else
+      await this.setState({
+        filtroMoradores2: true,
+        filtroMoradores3: false,
+        filtroMoradores4: false,
+        filtroMoradores5: false,
+        filtroMoradores6: false,
+      });
+  }
+
+  fMoradores3 = async (checked) => {
+    if (this.state.filtroMoradores3)
+      await this.setState({ filtroMoradores3: false });
+    else
+      await this.setState({
+        filtroMoradores3: true,
+        filtroMoradores2: false,
+        filtroMoradores4: false,
+        filtroMoradores5: false,
+        filtroMoradores6: false,
+      });
+  }
+
+  fMoradores4 = async (checked) => {
+    if (this.state.filtroMoradores4)
+      await this.setState({ filtroMoradores4: false });
+    else
+      await this.setState({
+        filtroMoradores4: true,
+        filtroMoradores3: false,
+        filtroMoradores2: false,
+        filtroMoradores5: false,
+        filtroMoradores6: false,
+      });
+  }
+
+  fMoradores5 = async (checked) => {
+    if (this.state.filtroMoradores5)
+      await this.setState({ filtroMoradores5: false });
+    else
+      await this.setState({
+        filtroMoradores5: true,
+        filtroMoradores3: false,
+        filtroMoradores4: false,
+        filtroMoradores2: false,
+        filtroMoradores6: false,
+      });
+  }
+
+  fMoradores6 = async (checked) => {
+    if (this.state.filtroMoradores6)
+      await this.setState({ filtroMoradores6: false });
+    else
+      await this.setState({
+        filtroMoradores6: true,
+        filtroMoradores3: false,
+        filtroMoradores4: false,
+        filtroMoradores5: false,
+        filtroMoradores2: false,
+      });
+  }
+
+  fVagas1 = async (checked) => {
+    if (this.state.filtroVagas1)
+      await this.setState({ filtroVagas1: false });
+    else
+      await this.setState({
+        filtroVagas1: true,
+        filtroVagas2: false,
+        filtroVagas3: false,
+      });
+  }
+
+  fVagas2 = async (checked) => {
+    if (this.state.filtroVagas2)
+      await this.setState({ filtroVagas2: false });
+    else
+      await this.setState({
+        filtroVagas2: true,
+        filtroVagas1: false,
+        filtroVagas3: false,
+      });
+  }
+
+  fVagas3 = async (checked) => {
+    if (this.state.filtroVagas3)
+      await this.setState({ filtroVagas3: false });
+    else
+      await this.setState({
+        filtroVagas3: true,
+        filtroVagas2: false,
+        filtroVagas1: false,
+      });
+  }
+  
+  valMenor = async (text) => {
+    if (text){      
+      await this.setState({ filtroValorMenor: true});
+      aluguelMin = text;
+      console.log(aluguelMin);
+    }
+  }
+
+  valMaior = async (text) => {
+    if (text){
+     aluguelMax = text;
+      await this.setState({ filtroValorMaior: true});
+    }
+  }
+ 
+  filtro = async () => {
+   await this.setState({ listaRepublicas: this.state.fullData })
+    let listaRepublicas = this.state.listaRepublicas;
+    console.log(listaRepublicas);
+    console.log(this.state.listaRepublicas);
+    console.log(this.state.aluguelMin);
+    console.log(this.state.aluguelMax);
+    console.log(this.state.fullData);
+    console.log(this.state.filtroValorMenor);
+    
+    if ((this.state.filtroAnimalSim) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "animal": "Sim" });
+    }
+    if ((this.state.filtroAnimalNao) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "animal": "Nao" });
+    }
+    if ((this.state.filtroMasc) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "genero": "Masculina" });
+    }
+    if ((this.state.filtroFem) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "genero": "Feminina" });
+    }
+    if ((this.state.filtroMista) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "genero": "Mista" });
+    }
+    if ((this.state.filtroMoradores2) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "pessoas": "2" });
+    }
+    if ((this.state.filtroMoradores3) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "pessoas": "3" });
+    }
+    if ((this.state.filtroMoradores4) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "pessoas": "4" });
+    }
+    if ((this.state.filtroMoradores5) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "pessoas": "5" });
+    }
+    if ((this.state.filtroMoradores6) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "pessoas": "6+" });
+    }
+    if ((this.state.filtroVagas1) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "numVagas": "1" });
+    }
+    if ((this.state.filtroVagas2) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "numVagas": "2" });
+    }
+    if ((this.state.filtroVagas3) === true) {
+      listaRepublicas = _.filter(this.state.listaRepublicas, { "numVagas": "3" });
+    }
+    if ((this.state.filtroValorMenor) === true) {
+     listaRepublicas = _.filter(listaRepublicas, ({ valorAluguel }) => valorAluguel >= aluguelMin);
+    }
+    if ((this.state.filtroValorMaior) === true) {
+     listaRepublicas = _.filter(listaRepublicas, ({ valorAluguel }) => valorAluguel <= aluguelMax);
+    }
+    await this.setState({ listaRepublicas, filtroValorMenor: false, filtroValorMaior: false });
+    aluguelMin = null;
+    aluguelMax = null;
+
+  };
   render() {
     return (
       <View>
@@ -38,7 +275,122 @@ class Republica extends Component {
             keyExtractor={item => item._id}
           />
         </ScrollView>
-      </View>
+        <View style={{
+          backgroundColor: "#00000080",
+          flex: 1
+        }}>
+          <Modal
+            animationType="fade"
+            visible={this.state.modalVisible}
+            transparent={true}
+          >
+            <View style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 500,
+              marginTop: 125,
+              marginHorizontal: 50,
+              backgroundColor: "white",
+              borderRadius: 20,
+              alignItems: "center",
+              shadowColor: "#000",
+              shadowOffset: {
+                width: 0,
+                height: 2
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+              elevation: 5
+            }}>
+              <Text>Valor</Text>
+              <ListItem style={{ alignItems: "stretch", marginBottom: 10 }}>
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >De</Text>
+                <Item underlined style={{width: 100, borderBottomColor: 'rgba(29,161,242,1)'}}>
+                  <Input style={{ alignSelf: "stretch" }} onChangeText={text => this.valMenor(text)}/>
+                </Item>
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >Até</Text>
+                <Item underlined style={{width: 100,borderBottomColor: 'rgba(29,161,242,1)'}} >
+                  <Input style={{ alignSelf: "stretch" }} onChangeText={text => this.valMaior(text)} />
+                </Item>
+              </ListItem>
+              <Text>Aceita animais?</Text>
+              <ListItem style={{ alignItems: "center", marginBottom: 10 }}>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fAnimalSim} checked={this.state.filtroAnimalSim} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >Sim</Text>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fAnimalNao} checked={this.state.filtroAnimalNao} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >Não</Text>
+              </ListItem>
+              <Text> Tipo de república</Text>
+              <ListItem style={{ alignItems: "center", marginBottom: 10 }}>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fMasc} checked={this.state.filtroMasc} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >Masculina</Text>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fFem} checked={this.state.filtroFem} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >Feminina</Text>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fMista} checked={this.state.filtroMista} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >Mista</Text>
+              </ListItem>
+              <Text>Capacidade de moradores</Text>
+              <ListItem style={{ alignItems: "center", marginBottom: 10 }}>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fMoradores2} checked={this.state.filtroMoradores2} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >2</Text>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fMoradores3} checked={this.state.filtroMoradores3} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >3</Text>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fMoradores4} checked={this.state.filtroMoradores4} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >4</Text>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fMoradores5} checked={this.state.filtroMoradores5} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >5</Text>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fMoradores6} checked={this.state.filtroMoradores6} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >6+</Text>
+              </ListItem>
+              <Text>Vagas disponíveis</Text>
+              <ListItem style={{ alignItems: "stretch", marginBottom: 10 }}>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fVagas1} checked={this.state.filtroVagas1} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >1</Text>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fVagas2} checked={this.state.filtroVagas2} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >2</Text>
+                <CheckBox style={{ alignSelf: "stretch" }} onPress={this.fVagas3} checked={this.state.filtroVagas3} />
+                <Text style={{ alignSelf: "stretch", paddingHorizontal: 15 }} >3</Text>
+              </ListItem>             
+
+              <TouchableOpacity
+                style={{
+                  alignSelf: 'center',
+                  backgroundColor: "#30C21E",
+                  borderRadius: 20,
+                  padding: 10,
+                  elevation: 2,
+                  justifyContent: "center",
+                  height: 45,
+                  width: 170
+                }}
+                onPress={() => {
+                  this.setState({ modalVisible: false });
+                  this.filtro();
+                  console.log(this.state.listaRepublicas);
+                }}
+              >
+                <Text style={{
+                  color: '#ffffff',
+                  fontFamily: 'Roboto',
+                  textAlign: 'center',
+                  fontSize: 20,
+                }}>Fechar</Text>
+              </TouchableOpacity>
+            </View>
+          </Modal>
+        </View>
+        <Fab
+          direction="up"
+          containerStyle={{}}
+          style={{ backgroundColor: 'rgba(29,161,242,1)' }}
+          position="bottomLeft"
+          onPress={() => {
+            this.setState({ modalVisible: true });
+          }}
+        >
+          <Icon name="md-add" />
+        </Fab>
+      </View >
     );
   }
 }
