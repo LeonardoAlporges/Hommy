e385dcb90894939a5209df667856837c0f589bbe
Leonardo <leonardo.martins@itixti.com.br>
2020-08-29 20:00:06 -0300

    Refatoração estilo da tela de cadastro de carona

--- src/pages/Carona/CadastroCarona/style.js
+++ src/pages/Carona/CadastroCarona/style.js
@@ -1,5 +1,43 @@
+import styled from 'styled-components/native';
+import { Button } from 'native-base';
 import { StyleSheet } from 'react-native';
 
+export const FieldSet = styled.View`
+  width: 45%;
+  height: 50px;
+  border-radius: 3;
+  border-width: 1px;
+  align-items: center;
+  border-color: #dcdcdc;
+  padding-top: 5px;
+`;
+
+export const FieldSetLarge = styled.View`
+  width: 100%;
+  height: 50px;
+  border-radius: 3;
+  border-width: 1px;
+  align-items: center;
+  border-color: #dcdcdc;
+  padding-top: 5px;
+`;
+export const LabelFielSet = styled.Text`
+  position: absolute;
+  top: -13px;
+  left: 10px;
+  font-family: WorkSans;
+  font-size: 16px;
+  color: #022250;
+  background-color: #ffffff;
+  padding: 0 5px 0 5px;
+`;
+export const Linha = styled.View`
+  height: 50px;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 33px;
+`;
 const estilo = StyleSheet.create({
   V_header: {
     display: 'flex',
@@ -10,45 +48,45 @@ const estilo = StyleSheet.create({
     borderBottomWidth: 2,
     borderBottomColor: '#142850',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'flex-start'
   },
   campos: {
-    marginTop: 20,
+    marginTop: 20
   },
   iconHeader: {
     fontSize: 26,
     color: '#142850',
-    marginHorizontal: '5%',
+    marginHorizontal: '5%'
   },
   V_modal: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   V_interna1: {
     width: 50,
     height: 50,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(52, 52, 52, 0.1)',
+    backgroundColor: 'rgba(52, 52, 52, 0.1)'
   },
   txtLabel: {
     fontFamily: 'Roboto',
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#586880',
+    color: '#586880'
   },
   title: {
     fontSize: 24,
     fontFamily: 'Roboto',
     fontWeight: 'bold',
-    color: '#142850',
+    color: '#142850'
   },
   V_Conteudo: {
     height: '100%',
     width: '100%',
     paddingVertical: 15,
-    paddingHorizontal: 20,
+    paddingHorizontal: 20
   },
   ruaNum: {
     display: 'flex',
@@ -56,7 +94,7 @@ const estilo = StyleSheet.create({
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginTop: 40,
+    marginTop: 40
   },
   Valores: {
     display: 'flex',
@@ -64,61 +102,74 @@ const estilo = StyleSheet.create({
     width: '100%',
     flexDirection: 'row',
     backgroundColor: '#f8f2',
-    justifyContent: 'space-between',
+    justifyContent: 'space-between'
   },
   campos2: {
-    marginTop: 20,
+    marginTop: 20
   },
   place: {
     fontFamily: 'Roboto',
     fontSize: 14,
-    color: '#989898',
+    color: '#989898'
   },
   enviar: {
     alignItems: 'center',
     marginTop: 30,
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    justifyContent: 'space-evenly'
   },
   btnProximo: {
-    borderRadius: 7,
+    borderRadius: 3,
     justifyContent: 'center',
     alignItems: 'center',
     width: '80%',
     height: 50,
-    backgroundColor: '#142850',
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7
+    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+
+    elevation: 15
   },
   iconeBtn: {
     margin: 0,
-    color: '#68c0e8',
+    color: '#68c0e8'
   },
   botaoEnv: {
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#142850',
-    color: '#fff',
+    color: '#fff'
   },
   txtCarona: {
     fontFamily: 'Roboto',
     fontSize: 14,
     color: '#687368',
-    marginHorizontal: 20,
+    marginHorizontal: 20
   },
   rowStyle: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginTop: 15,
+    marginTop: 15
   },
   campoStyle: {
-    width: '43%',
+    width: '43%'
   },
   V_erro: {
-    height: 15,
+    height: 12,
+    width: '100%',
+    textAlign: 'left'
   },
   textError: {
+    fontFamily: 'WorkSans',
+    textAlign: 'left',
     fontSize: 10,
-    color: 'red',
+    color: 'red'
   },
   V_btn: {
     width: '100%',
@@ -126,14 +177,14 @@ const estilo = StyleSheet.create({
     display: 'flex',
     alignContent: 'center',
     paddingLeft: '20%',
-    marginTop: '10%',
+    marginTop: '10%'
   },
   ViewFundo: {
     height: '100%',
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#00000080',
+    backgroundColor: '#00000080'
   },
   ViewModal: {
     backgroundColor: 'rgba(52, 52, 52, 0)',
@@ -141,14 +192,14 @@ const estilo = StyleSheet.create({
     alignItems: 'center',
     height: 50,
     marginHorizontal: 30,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   InputHora: {
     width: 130,
     height: 50,
     justifyContent: 'center',
-    alignItems: 'center',
-  },
+    alignItems: 'center'
+  }
 });
 
 export default estilo;
