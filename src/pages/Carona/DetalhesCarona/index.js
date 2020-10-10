56644ffcffd89a370e3a184b4659a9437693d14a
Leonardo <leonardofixaa@gmail.com>
2020-10-10 12:50:07 -0300

    Alguns ajustes na carona

--- src/pages/Carona/DetalhesCarona/index.js
+++ src/pages/Carona/DetalhesCarona/index.js
@@ -1,22 +1,50 @@
 import React, { useState } from 'react';
 import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
-import Estilo from './style';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Button } from 'native-base';
+import { useSelector } from 'react-redux';
+
 import CustomModal from '../../../components/Alert';
 import api from '../../../service/api';
 import Loading from '../../../components/Loading';
-import { useSelector } from 'react-redux';
+
+import Estilo from './style';
+
+import {
+  Header,
+  HeaderUser,
+  Imagem,
+  InformacaoPerfil,
+  Nome,
+  ViewNotas,
+  Nota,
+  ContainerIformacao,
+  ViewData,
+  Data,
+  ViewPartida,
+  Partida,
+  ViewHora,
+  Hora,
+  ViewValor,
+  LabelValor,
+  Valor,
+  BarraSeparadora,
+  ViewPontos,
+  ViewPontoEmbarque,
+  LabelPontoEmbarque,
+  ViewLabel,
+  TextoLabel
+} from './style';
 
 export function DetalhesCarona({ navigation }) {
   const moment = require('moment');
   moment.locale('pt', {
     months: 'Janeiro_Fevereiro_Março_Abril_Maio_Junho_Julho_Agosto_Setembro_Outubro_Novembro_Dezembro'.split('_'),
-    weekdays: 'Domingo_Segunda_Terça_Quarta_Quinta_Sexta_Sabado'.split('_'),
+    weekdays: 'Domingo_Segunda_Terça_Quarta_Quinta_Sexta_Sabado'.split('_')
   });
   const [botaoInteresse, setBotaoInteresse] = useState(navigation.state.params.desativarBotaoInteresse);
-  const [dados, setDados] = useState(navigation.state.params.dados); 
+  const [dados, setDados] = useState(navigation.state.params.dados);
 
   const email = useSelector(state => state.user.email);
   const [sucesso, setSucesso] = useState(false);
@@ -28,134 +56,125 @@ export function DetalhesCarona({ navigation }) {
 
   async function demonstrarInteresse() {
     setLoading(true);
-    const dado = { email: email};
-    console.log("DAODS:",dado,dados)
+    const dado = { email: email }; //ALTERAR ISSO PARA RECEBER SOMENTE O EMAIL
+
     await api
       .put(`/carona/meusInteresses/${dados._id}`, dado)
       .then(Response => {
         setSucesso(true);
         setLoading(false);
       })
-      .catch(e => {
+      .catch(erro => {
         setErro(true);
         setLoading(false);
       });
-    setLoading(false);
   }
 
   return (
     <ScrollView style={{ paddingBottom: 50 }}>
       {loading && <Loading />}
       {sucesso && (
-        <View style={Estilo.V_modal}>
-          <CustomModal
-            parametro="Custom"
-            titulo="Obrigado pelo interesse"
-            descricao="Você será adicionado a uma lista de interesse e será notificado assim que o ofertante confirmar sua vaga."
-            botao="Confirmar"
-            callback={() => {
-              navigation.navigate('Viagens');
-            }}
-          />
-        </View>
+        <CustomModal
+          parametro="Custom"
+          titulo="Obrigado pelo interesse"
+          descricao="Você será adicionado a uma lista de interesse e será notificado assim que o ofertante confirmar sua vaga."
+          botao="Confirmar"
+          callback={() => {
+            navigation.navigate('Viagens');
+          }}
+        />
       )}
       {erro && (
-        <View style={Estilo.V_modal}>
-          <CustomModal
-            parametro="Erro"
-            descricao="Você já tem um agendamento cadastrado nessa carona."
-            callback={() => {
-              setErro(false);
-            }}
-          />
-        </View>
+        <CustomModal
+          parametro="Erro"
+          descricao="Você já tem um agendamento cadastrado nessa carona."
+          callback={() => {
+            setErro(false);
+          }}
+        />
       )}
-      <View style={Estilo.V_Margin}>
-        <View style={Estilo.header}>
-          <TouchableOpacity
+
+      <Header>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack(null);
+          }}
+        >
+          <Icon style={{ fontSize: 30, color: '#FFFFFF' }} name="arrow-left" />
+        </TouchableOpacity>
+      </Header>
+
+      <HeaderUser>
+        <Imagem
+          source={{
+            uri: dados.imagem
+          }}
+        />
+        <InformacaoPerfil>
+          <Nome>{dados.nome} </Nome>
+          <ViewNotas>
+            <Icon style={{ fontSize: 20, color: '#FFFFFF', marginHorizontal: 3 }} name="star-outline" />
+            <Nota>{dados.nota}</Nota>
+          </ViewNotas>
+        </InformacaoPerfil>
+      </HeaderUser>
+
+      <ContainerIformacao>
+        <ViewData>
+          <Data>{data}</Data>
+        </ViewData>
+        <ViewPartida>
+          <Partida>Saída</Partida>
+        </ViewPartida>
+        <ViewHora>
+          <Hora>
+            {horaSaida} - {dados.localSaida}
+          </Hora>
+        </ViewHora>
+        <ViewPartida>
+          <Partida>Previsão Chegada</Partida>
+        </ViewPartida>
+        <ViewHora>
+          <Hora>
+            {horaChegada} - {dados.localChegada}
+          </Hora>
+        </ViewHora>
+
+        <ViewValor>
+          <LabelValor>Preço para 1 passageiro</LabelValor>
+          <Valor>R$ {dados.valor},00</Valor>
+        </ViewValor>
+      </ContainerIformacao>
+      <BarraSeparadora />
+
+      <ViewPontos>
+        <ViewPontoEmbarque>
+          <LabelPontoEmbarque>Ponto de Embarque</LabelPontoEmbarque>
+        </ViewPontoEmbarque>
+        <ViewLabel>
+          <TextoLabel>•{dados.embarque}</TextoLabel>
+        </ViewLabel>
+        <ViewPontoEmbarque>
+          <LabelPontoEmbarque>Ponto Final</LabelPontoEmbarque>
+        </ViewPontoEmbarque>
+        <ViewLabel>
+          <TextoLabel>•{dados.desembarque}</TextoLabel>
+        </ViewLabel>
+      </ViewPontos>
+      {!botaoInteresse ? (
+        <ViewBotao>
+          <Button
+            style={Estilo.Botao}
             onPress={() => {
-              navigation.goBack(null);
+              demonstrarInteresse();
             }}
           >
-            <Icon style={Estilo.iconHer} name="arrow-left" />
-          </TouchableOpacity>
-        </View>
-
-        <View style={Estilo.V_HeaderUser}>
-          <Image
-            source={{
-              uri: dados.imagem,
-            }}
-            style={Estilo.V_imagem}
-          />
-          <View style={Estilo.V_Nome}>
-            <Text style={Estilo.Nome}>{dados.nome} </Text>
-            <View style={Estilo.V_nota}>
-              <Icon style={Estilo.iconNota} name="star-outline" />
-              <Text style={Estilo.Nota}>{dados.nota}</Text>
-            </View>
-          </View>
-          <View />
-        </View>
-
-        <View style={Estilo.V_Infor}>
-          <View style={Estilo.V_data}>
-            <Text style={Estilo.data}>{data}</Text>
-          </View>
-          <View style={Estilo.V_partida}>
-            <Text style={Estilo.txtPartida}>Saída</Text>
-          </View>
-          <View style={Estilo.V_Hora}>
-            <Text style={Estilo.Txthora}>
-              {horaSaida} - {dados.localSaida}
-            </Text>
-          </View>
-          <View style={Estilo.V_partida}>
-            <Text style={Estilo.txtPartida}>Previsão Chegada</Text>
-          </View>
-          <View style={Estilo.V_Hora}>
-            <Text style={Estilo.Txthora}>
-              {horaChegada} - {dados.localChegada}
-            </Text>
-          </View>
-
-          <View style={Estilo.V_valor}>
-            <Text style={Estilo.Txtvalor1}>Preço para 1 passageiro</Text>
-            <Text style={Estilo.Txtvalor}>R$ {dados.valor},00</Text>
-          </View>
-        </View>
-        <View style={Estilo.barra} />
-        <View style={Estilo.Pontos}>
-          <View style={Estilo.V_ptEm}>
-            <Text style={Estilo.T_ptEm}>Ponto de Embarque</Text>
-          </View>
-          <View style={Estilo.V_label}>
-            <Text style={Estilo.T_label}>•{dados.embarque}</Text>
-          </View>
-          <View style={Estilo.V_ptEm}>
-            <Text style={Estilo.T_ptEm}>Ponto Final</Text>
-          </View>
-          <View style={Estilo.V_label}>
-            <Text style={Estilo.T_label}>•{dados.desembarque}</Text>
-          </View>
-        </View>
-        {!botaoInteresse ? (
-          <View style={Estilo.ViewButon}>
-            <View style={Estilo.V_Btn}>
-              <Button
-                style={Estilo.Botao}
-                onPress={() => {
-                  demonstrarInteresse();
-                }}
-              >
-                <Text style={Estilo.txtBotao}>Tenho Interesse</Text>
-              </Button>
-            </View>
-          </View>
-        ) : (
-            <View style={{ height: 40 }} />
-          )}
-      </View>
+            <Text style={Estilo.txtBotao}>Tenho Interesse</Text>
+          </Button>
+        </ViewBotao>
+      ) : (
+        <View style={{ height: 40 }} />
+      )}
     </ScrollView>
   );
 }
